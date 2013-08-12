8cfda57e038511eadb96df9dde72463171e89ca3
Abrar <ahmeda8@outlook.com>
2013-08-12 01:13:18 -0700

    binding error

--- driverDashWp7/driverDashWp7/ViewModels/MainPageViewModel.cs
+++ driverDashWp7/driverDashWp7/ViewModels/MainPageViewModel.cs
@@ -13,18 +13,18 @@ namespace driverDashWp7.ViewModels
         public MainPageViewModel()
         {
             db = DatabaseModel.GetInstance(App.DB_CONNECTION);
-            CarCollection = new ObservableCollection<CarViewModel>();
+            Cars = new ObservableCollection<CarViewModel>();
 
             var allCarsQuery = from carInfo c in db.carInfo
                                select c;
             foreach (carInfo c in allCarsQuery.ToArray())
             {
-                CarCollection.Add(new CarViewModel { ID=c.CarID, Make=c.CarMake, Lic=c.CarLic, Model=c.CarModel, Year = c.CarYear });
+                Cars.Add(new CarViewModel { ID=c.CarID, Make=c.CarMake, Lic=c.CarLic, Model=c.CarModel, Year = c.CarYear });
             }
         }
 
         private ObservableCollection<CarViewModel> _carCollection;
-        public ObservableCollection<CarViewModel> CarCollection
+        public ObservableCollection<CarViewModel> Cars
         {
             get { return _carCollection; }
             set
